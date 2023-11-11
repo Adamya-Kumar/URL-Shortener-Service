3eda6569418d775d077787c05c22432ec1d6d61e
Adamya Kumar <101924838+Adamya-Kumar@users.noreply.github.com>
2023-11-11 14:24:19 +0530

    Add files via upload

---
+++ models/url.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    shortId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    redirectURL: {
+      type: String,
+      required: true,
+    },
+    visitHistory: [{ timestamp: { type: Number } }],
+  },
+  { timestamps: true }
+);
+
+const Users = mongoose.model("Url", UserSchema);
+
+module.exports = Users;
